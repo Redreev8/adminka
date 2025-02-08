5220b9e363f06036c9d80e88c7fad65c7bc22a33
redreev8 <redreevs@mail.ru>
2025-02-08 19:33:28 +0500

    change useBorder & position-child-to-parent func to file

---
+++ src/helper/position-child-to-parent.ts
@@ -0,0 +1,18 @@
+'use client' 
+
+const positionChildToParent = (parent: HTMLElement, child: HTMLElement) => {
+    const parentPos = parent.getBoundingClientRect()
+    const childPos = child.getBoundingClientRect()
+    return {
+        top: childPos.top - parentPos.top,
+        right: childPos.right - parentPos.right * -1,
+        bottom: childPos.bottom - parentPos.bottom * -1,
+        left: childPos.left - parentPos.left,
+        height: childPos.height,
+        width: childPos.width,
+        parentHeight: parentPos.height,
+        parentWidth: parentPos.width,
+    }
+}
+
+export default positionChildToParent
