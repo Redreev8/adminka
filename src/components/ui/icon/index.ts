a8e6051d6d8b99caf3fe39e6a06074ed66bf8830
redreev8 <redreevs@mail.ru>
2025-02-10 00:14:02 +0500

    add icons

---
+++ src/components/ui/icon/index.ts
@@ -0,0 +1,12 @@
+import { ForwardRefExoticComponent, RefAttributes, SVGAttributes } from "react"
+import EditIcon from "./edit"
+import TrashIcon from "./trash"
+
+export type ForwardType = ForwardRefExoticComponent<SVGAttributes<SVGSVGElement> & RefAttributes<SVGSVGElement>>
+
+const icons = {
+    EditIcon,
+    TrashIcon
+}
+
+export default icons
