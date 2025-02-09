81a234bda8a9ab4a382c7945473c15f7551c4596
redreev8 <redreevs@mail.ru>
2025-02-10 00:21:15 +0500

    container inhiret min-height

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -13,7 +13,7 @@ const Container = forwardRef<
         className,
         '2xl:max-w-[1600px] 2xl:w-full 2xl:mx-auto',
         'lg:w-[calc(100%-80px)] lg:mx-10',
-        'w-[calc(100%-40px)] mx-5',
+        'w-[calc(100%-40px)] min-h-[inherit] mx-5',
         'border-x border-b border-label',
     )
     const refContainer = useUniteRef<HTMLDivElement>(ref)
