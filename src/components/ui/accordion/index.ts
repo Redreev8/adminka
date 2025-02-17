c191263c91445152b3d9d36c28338582f6b9b6eb
Redreev8 <redreevs@mail.ru>
2025-02-17 20:05:31 +0500

    add import AccordionList

--- src/components/ui/accordion/index.ts
+++ src/components/ui/accordion/index.ts
@@ -1,3 +1,4 @@
 export { default as Accordion } from './accordion'
+export { default as AccordionList } from './accordion-list'
 export { default as AccordionTitle } from './accordion-title'
 export { default as AccordionContent } from './accordion-content'
